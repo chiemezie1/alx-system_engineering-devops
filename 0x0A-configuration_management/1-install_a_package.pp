a783814520c2c07ecb638293092b5c5344372936
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-01 15:34:26 +0100

    ./1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-# Install Flask version 2.1.0 using pip3
+# A puppet file thst install flask from pip3
+# Define a package resource
+
 package { "Flask":
   ensure   => "2.1.0",
   provider => "pip3",
