4b9f1035f2b638842863918d01c0e18e66a283ce
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-01 16:14:22 +0100

    ./1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# A Puppet manifest to install Flask version 2.1.0 using pip3
-
-exec { 'install_flask':
-  command => 'pip3 install flask==2.1.0',  # Command to install Flask version 2.1.0
-  path    => ['/usr/bin', '/bin'],         # The path to look for the command
-  unless  => 'pip3 show flask | grep Version | grep -q 2.1.0', # Only run if Flask 2.1.0 is not already installed
+# 1-install_a_package.pp
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
