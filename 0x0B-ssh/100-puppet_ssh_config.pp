385708f391438539d4eee8dbd41e52dfc6ca0541
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-04 01:27:38 +0100

    ./100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,32 +1,32 @@
-# File: 100-puppet_ssh_config.pp
-
-# Ensure SSH client config directory exists
-file { '/home/ubuntu/.ssh':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+# Ensure the SSH configuration directory exists
+file { '~/.ssh':
+  ensure => 'directory',
+  mode   => '0700',
+  owner  => $USER,
+  group  => $USER,
 }
 
-# Ensure SSH client config file exists
-file { '/home/ubuntu/.ssh/config':
-  ensure => file,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0644',
+# Ensure the SSH configuration file exists
+file { '~/.ssh/config':
+  ensure  => 'file',
+  mode    => '0600',
+  owner   => $USER,
+  group   => $USER,
+  require => File['~/.ssh'],
 }
 
-# Configure SSH client to use specific private key
+# Configure the SSH client to use the specified private key
 file_line { 'Declare identity file':
-  ensure  => present,
-  path    => '/home/ubuntu/.ssh/config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^#*IdentityFile.*',
+  path  => '~/.ssh/config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^IdentityFile',
+  require => File['~/.ssh/config'],
 }
 
 # Disable password authentication
 file_line { 'Turn off passwd auth':
-  ensure  => present,
-  path    => '/home/ubuntu/.ssh/config',
-  line    => 'PasswordAuthentication no',
-  match   => '^#*PasswordAuthentication.*',
+  path  => '~/.ssh/config',
+  line  => 'PasswordAuthentication no',
+  match => '^PasswordAuthentication',
+  require => File['~/.ssh/config'],
 }
