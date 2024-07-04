1b068d92e1fbe316302fdb9dee6ca86cdf440616
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-04 01:20:27 +0100

    ./100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,25 @@
+# File: 100-puppet_ssh_config.pp
+
+# Ensure SSH client config file exists
+file { '/home/ubuntu/.ssh/config':
+  ensure => file,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0644',
+}
+
+# Configure SSH client to use specific private key
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/home/ubuntu/.ssh/config',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^#*IdentityFile.*',
+}
+
+# Disable password authentication
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/home/ubuntu/.ssh/config',
+  line    => 'PasswordAuthentication no',
+  match   => '^#*PasswordAuthentication.*',
+}
