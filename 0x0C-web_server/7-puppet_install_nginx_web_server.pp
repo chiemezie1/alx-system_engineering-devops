3ccb7325469ddce7bbf605a638211b32c4174e55
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-11 13:05:53 +0100

    ./7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,42 +1,50 @@
-# configures server using puppet
+# Puppet manifest to install and configure Nginx web server
 
-# Update the package list
-exec { 'apt-get update':
-  command => '/usr/bin/env apt-get -y update',
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
 }
 
-# Install Nginx
-exec { 'apt-get install nginx':
-  command => '/usr/bin/env apt-get -y install nginx',
-  require => Exec['apt-get update'],
+# Define default Nginx site configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => '
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+
+      root /var/www/html;
+      index index.html index.htm index.nginx-debian.html;
+
+      # Serve Hello World! on root /
+      location / {
+        echo "Hello World!";
+      }
+
+      # Redirect /redirect_me with 301 Moved Permanently
+      location /redirect_me {
+        return 301 https://github.com/chiemezie1;
+      }
+
+      # Custom 404 page configuration
+      error_page 404 /custom_404.html;
+      location = /custom_404.html {
+        echo "Ceci n\'est pas une page";
+        internal;
+      }
+    }
+  ',
 }
 
-# Create the Hello World index file
-exec { 'create index.html':
-  command => '/usr/bin/env echo "Hello World!" > /var/www/html/index.nginx-debian.html',
-  require => Exec['apt-get install nginx'],
+# Create custom 404 page
+file { '/var/www/html/custom_404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page",
 }
 
-# Insert the redirect configuration into the Nginx default site configuration
-exec { 'configure redirect':
-  command => '/usr/bin/env sed -i "/server_name _;/ a\\location /redirect_me { return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; }" /etc/nginx/sites-available/default',
-  require => Exec['create index.html'],
-}
-
-# Insert the custom 404 error page configuration into the Nginx default site configuration
-exec { 'configure 404':
-  command => '/usr/bin/env sed -i "/server_name _;/ a\\error_page 404 /custom_404.html;" /etc/nginx/sites-available/default',
-  require => Exec['configure redirect'],
-}
-
-# Create the custom 404 error page file
-exec { 'create 404.html':
-  command => '/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html',
-  require => Exec['configure 404'],
-}
-
-# Restart Nginx to apply the new configuration
-exec { 'restart nginx':
-  command => '/usr/bin/env systemctl restart nginx',
-  require => Exec['create 404.html'],
+# Restart Nginx service to apply changes
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
