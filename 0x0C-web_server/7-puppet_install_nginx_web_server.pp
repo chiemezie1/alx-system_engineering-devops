a2854370ede343e45d95bc0bf434767c46bb1772
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-11 13:17:59 +0100

    ./7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,50 +1,56 @@
-# Puppet manifest to install and configure Nginx web server
+# File: 7-puppet_install_nginx_web_server.pp
+# Install and configure Nginx web server with Puppet
 
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
-}
+class nginx_install {
+  # Ensure Nginx package is installed
+  package { 'nginx':
+    ensure => installed,
+  }
 
-# Define default Nginx site configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => '
-    server {
-      listen 80 default_server;
-      listen [::]:80 default_server;
+  # Define the default Nginx site configuration
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => '
+      server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-      root /var/www/html;
-      index index.html index.htm index.nginx-debian.html;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
 
-      # Serve Hello World! on root /
-      location / {
-        echo "Hello World!";
+        # Serve "Hello World!" on root /
+        location / {
+          echo "Hello World!";
+        }
+
+        # Redirect /redirect_me with 301 Moved Permanently
+        location /redirect_me {
+          return 301 https://github.com/chiemezie1;
+        }
+
+        # Custom 404 page configuration
+        error_page 404 /custom_404.html;
+        location = /custom_404.html {
+          echo "Ceci n\'est pas une page";
+          internal;
+        }
       }
+    ',
+  }
 
-      # Redirect /redirect_me with 301 Moved Permanently
-      location /redirect_me {
-        return 301 https://github.com/chiemezie1;
-      }
+  # Create the custom 404 page
+  file { '/var/www/html/custom_404.html':
+    ensure  => file,
+    content => "Ceci n'est pas une page",
+  }
 
-      # Custom 404 page configuration
-      error_page 404 /custom_404.html;
-      location = /custom_404.html {
-        echo "Ceci n\'est pas une page";
-        internal;
-      }
-    }
-  ',
+  # Restart Nginx service to apply changes
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/nginx/sites-available/default'],
+  }
 }
 
-# Create custom 404 page
-file { '/var/www/html/custom_404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page",
-}
-
-# Restart Nginx service to apply changes
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
-}
+# Apply the nginx_install class to install and configure Nginx
+include nginx_install
