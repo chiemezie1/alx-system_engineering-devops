1bc41b0ed5da72f79e6673e95f9155755d26795f
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-11 09:49:22 +0100

    ./7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,9 +1,62 @@
-# configures server using puppet
+# A Puppet module to install and configure Nginx web server
 
-exec {'/usr/bin/env apt-get -y update': }
-exec {'/usr/bin/env apt-get -y install nginx': }
-exec {'/usr/bin/env echo "Hello World" > /var/www/html/index.nginx-debian.html': }
-exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html': }
-exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://github.com/chiemezie1 permanent;" /etc/nginx/sites-available/default': }
-exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
-exec {'/usr/bin/env service nginx start': }
+# Ensure Nginx is installed
+package { 'nginx':
+  ensure => installed,
+}
+
+# Ensure Nginx service is enabled and running
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  require    => Package['nginx'],
+}
+
+# Define the content of the index.html file
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+# Define the content of the custom 404 error page
+file { '/var/www/html/custom_404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page",
+  require => Package['nginx'],
+}
+
+# Create an Nginx configuration file
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  require => [Package['nginx'], File['/var/www/html/index.html'], File['/var/www/html/custom_404.html']],
+  notify  => Service['nginx'],
+}
+
+# Create the template for the Nginx configuration
+file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
+  ensure  => file,
+  content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    server_name _;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location /redirect_me {
+        rewrite ^/redirect_me https://github.com/chiemezie1 permanent;
+    }
+
+    error_page 404 /custom_404.html;
+}
+  ',
+  require => Package['nginx'],
+}
