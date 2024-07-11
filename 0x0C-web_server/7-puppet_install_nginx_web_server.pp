462d4b7f3bb639cb6b8f5a1c17dd89a06dfdbdc7
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-11 09:39:10 +0100

    ./7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,63 +1,9 @@
-# Install and configure Nginx with Puppet
+# configures server using puppet
 
-# Ensure the package 'nginx' is installed
-package { 'nginx':
-  ensure => installed,
-}
-
-# Ensure the Nginx service is running and enabled to start at boot
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  require    => Package['nginx'],
-}
-
-# Create the index.html file with "Hello World!" content
-file { '/var/www/html/index.nginx-debian.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
-# Create the custom 404 error page
-file { '/var/www/html/custom_404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page",
-  require => Package['nginx'],
-}
-
-# Update the Nginx configuration file to set up the redirect and custom 404 page
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Template for the Nginx configuration file
-file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
-  ensure  => file,
-  content => '
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    error_page 404 /custom_404.html;
-
-    location / {
-        try_files $uri $uri/ =404;
-    }
-}
-  ',
-  require => Package['nginx'],
-}
+exec {'/usr/bin/env apt-get -y update': }
+exec {'/usr/bin/env apt-get -y install nginx': }
+exec {'/usr/bin/env echo "Hellow World" > /var/www/html/index.nginx-debian.html': }
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://github.com/chiemezie1 permanent;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html': }
+exec {'/usr/bin/env service nginx start': }
