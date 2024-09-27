aff0932882956727f3562bf52c25b0c89b003a1a
chiemezie1 <chiemezieagbo1@gmail.com>
2024-09-27 04:46:55 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-""" Exporting csv files"""
+"""Return number of subreddit subscribers"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    # Set up the URL and headers (with a custom User-Agent)
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'python:subreddit.subscriber.counter:v1.0 (by /u/your_username)'}
-    
-    try:
-        # Make a GET request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            # If the status code is not 200, return 0
-            return 0
-    except requests.RequestException:
-        # Handle any request exceptions by returning 0
+    """Returns the number of subreddit subscribers"""
+    URL = 'http://reddit.com/r/{}/about.json'.format(subreddit)
+    HEADERS = {'User-agent': 'Unix:0-subs:v1'}
+    response = requests.get(URL, headers=HEADERS)
+    if response.status_code != 200:
         return 0
+    return response.json().get('data', {}).get('subscribers', 0)
