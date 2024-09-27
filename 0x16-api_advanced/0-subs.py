2633d928a02bcc4271358fb74163cc43225cd37b
chiemezie1 <chiemezieagbo1@gmail.com>
2024-09-27 04:43:19 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """ Exporting csv files"""
-import json
 import requests
-import sys
-
 
 def number_of_subscribers(subreddit):
-    """Read reddit API and return number subscribers """
-    username = 'ledbag123'
-    password = 'Reddit72'
-    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
-    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    client = requests.session()
-    client.headers = headers
-    r = client.get(url, allow_redirects=False)
-    if r.status_code == 200:
-        return (r.json()["data"]["subscribers"])
-    else:
-        return(0)
+    # Set up the URL and headers (with a custom User-Agent)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'python:subreddit.subscriber.counter:v1.0 (by /u/your_username)'}
+    
+    try:
+        # Make a GET request to the Reddit API
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            # If the status code is not 200, return 0
+            return 0
+    except requests.RequestException:
+        # Handle any request exceptions by returning 0
+        return 0
